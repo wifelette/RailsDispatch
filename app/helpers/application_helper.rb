336d76455f671f69c96d9edac1e9db184ce1697e
Joseph Silvashy <jpsilvashy@gmail.com>
2010-04-01 14:26:53 -0700

    fixed helper for 'about one month ago' in sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   def weeks_or_time_ago_in_words(from_time, include_seconds = false)
     to_time   = Time.now
     weeks_ago = (to_time - from_time)/1.week
-    ["this week", "last week", "two weeks ago", "three weeks ago", "four weeks ago", "one month ago"][weeks_ago] || distance_of_time_in_words(from_time, Time.now, include_seconds)
+    ["this week", "last week", "two weeks ago", "three weeks ago", "four weeks ago", "one month ago", "about one month ago"][weeks_ago] || distance_of_time_in_words(from_time, Time.now, include_seconds)
   end
 
   def weeks_or_time_until_in_words(from_time, include_seconds = false)
