cbe8153b3687cf9bdb3686c713b899d7e4ba4f4f
Joseph Silvashy <jpsilvashy@gmail.com>
2010-04-01 14:48:53 -0700

    textarea for embed links becase strings aren't long enough

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,7 +3,7 @@ class Feed < ActiveRecord::Base
   belongs_to :contributor
   has_many :feed_entries, :dependent => :destroy
 
-  validates_presence_of :site_url, :feed_url, :contributor
+  validates_presence_of :site_url, :feed_url
 
   after_save :update_feed_entries
 
