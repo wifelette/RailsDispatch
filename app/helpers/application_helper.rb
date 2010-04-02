afe516e4978d19cc39806bc9e7915680b0a4212e
Joseph Silvashy <jpsilvashy@gmail.com>
2010-04-01 18:11:45 -0700

    fixed time_until helper for times longer than a week

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,8 +14,9 @@ module ApplicationHelper
 
   def weeks_or_time_until_in_words(from_time, include_seconds = false)
     to_time   = Time.now
-    weeks_ago = (to_time - from_time)/1.week
-    ["next week", "in two weeks", "in three weeks", "in four weeks", "in about a month", "upcoming"][weeks_ago] || distance_of_time_in_words(from_time, Time.now, include_seconds)
+    weeks_ago = (from_time - to_time)/1.week
+    # ["this week", "last week", "two weeks ago", "three weeks ago", "four weeks ago", "one month ago", "about one month ago"][weeks_ago] || distance_of_time_in_words(from_time, Time.now, include_seconds)
+    ["next week", "in about two weeks", "in three weeks", "in four weeks", "in about a month", "upcoming"][weeks_ago] || distance_of_time_in_words(from_time, Time.now, include_seconds)
   end
 
 end
