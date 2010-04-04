853b268b5390707b50ad5215cfe1b58f49b47afc
Joseph Silvashy <jpsilvashy@gmail.com>
2010-04-03 19:31:45 -0700

    awesome syntax coloring things and textile styles

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -3,14 +3,8 @@ class FeedEntry < ActiveRecord::Base
   belongs_to :feed, :dependent => :destroy
   
   def self.update_from_feed(feed_url, feed_id)
-    
     feed = Feedzirra::Feed.fetch_and_parse(feed_url)
-    
-    # feedTitle = feed.title
-    # parent = Feed.find(:feed_id => )
-    # parent.update_attribute :title, feedTitle
-    
-    add_entries(feed.entries, feed_id)    
+    add_entries(feed.entries, feed_id) if feed.updated?
   end
 
   # def self.update_from_feed_continuously(feed_url, delay_interval = 1.minutes)
@@ -19,7 +13,7 @@ class FeedEntry < ActiveRecord::Base
   #   loop do
   #     sleep delay_interval
   #     feed = Feedzirra::Feed.update(feed)
-  #     add_entries(feed.new_entries) if feed.updated?
+  #     add_entries(feed.new_entries)
   #   end
   # end
 
