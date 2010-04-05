46f175816b63cebcdf05f67f0894f8998cb2bd90
wycats <wycats@gmail.com>
2010-04-04 17:30:01 -0700

    Override #textilize to syntax highlight embedded <pre> tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,32 @@
+require "strscan"
+
 module ApplicationHelper
 
   def coderay(text)
-    text.gsub(/\<pre( lang="(.+?)" data-caption="(.+?)")?\>(.+?)\<\/pre\>/m) do
-      content_tag :div, :caption => $3, :class => "code_block" do
-        CodeRay.scan($4, $2).div(:css => :class)
+    text.gsub(/\<pre(?:\s+lang="(.+?)"(?:\s+data-caption="(.+?)")?)?\>(.+?)\<\/pre\>/m) do
+      contents = $3.strip
+      content_tag :div, :caption => $2, :class => "code_block" do
+        CodeRay.scan(contents, $1).div(:css => :class)
       end
     end
+  end
+
+  # Override textilize to syntax highlight code blocks in <pre>
+  def textilize(text)
+    output, current = "", ""
+    s = StringScanner.new(text)
+
+    while char = s.getch
+      current << char
+      if s.peek(4) == "<pre"
+        output << super(current)
+        current = ""
+        code = s.scan_until(%r{</pre>})
+        output << coderay(code)
+      end
+    end
+    output << super(current) unless current.blank?
+    output
   end
 
   def weeks_or_time_ago_in_words(from_time, include_seconds = false)
