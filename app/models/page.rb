d3167fca8aca1f5dfd9735e3b44510adb15335cc
Joseph Silvashy <jpsilvashy@gmail.com>
2010-04-05 11:06:38 -0700

    fixing validation in pages model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
   scope :published, where("published != ?", false)
 
-  validates_presence_of :title, :sidebar_title, :body, :publish_date, :slug
+  validates_presence_of :page_title, :sidebar_title, :body, :publish_date, :slug
       
   def slug=(text)
     write_attribute(:slug, text.parameterize)
