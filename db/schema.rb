684a8b9d5605f893c938defecc5987cc6a72b658
Joseph Silvashy <jpsilvashy@gmail.com>
2010-04-06 23:51:16 -0700

    adding banner and remove homepage place holder.

--- db/schema.rb
+++ db/schema.rb
@@ -139,16 +139,16 @@ ActiveRecord::Schema.define(:version => 20100405182251) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                             :null => false
-    t.string   "encrypted_password",   :limit => 40,                :null => false
-    t.string   "password_salt",                                     :null => false
+    t.string   "email",                              :null => false
+    t.string   "encrypted_password",   :limit => 40, :null => false
+    t.string   "password_salt",                      :null => false
     t.string   "confirmation_token",   :limit => 20
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "reset_password_token", :limit => 20
     t.string   "remember_token",       :limit => 20
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      :default => 0
+    t.integer  "sign_in_count"
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
