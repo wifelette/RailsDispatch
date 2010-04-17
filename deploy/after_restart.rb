bee737cc63059ace55926b03ec27396aa50349d6
wycats <wycats@gmail.com>
2010-04-16 23:08:28 -0400

    Maybe no bundle exec

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,2 +1,2 @@
 run "HOME=/home/deploy cd #{release_path} && bundle install"
-run "cd /data/engineyardrailsdispatch/current/ && bundle exec whenever --update-crontab"
+run "cd /data/engineyardrailsdispatch/current/ && whenever --update-crontab"
