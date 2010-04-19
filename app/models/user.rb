571449056a975c9ff69ff7d4d7ce94aef91ae600
Eric Saxby <esaxby@walmart.com>
2010-04-19 11:56:46 -0700

    User can't edit own admin status
    Simple form disabling that can be gotten around. Mostly you just
    don't want admins to destroy all admin access.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   #        :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :admin
   devise :authenticatable, :rememberable, :trackable, :registerable
   
   has_many :posts
