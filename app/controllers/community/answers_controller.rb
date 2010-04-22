619786fa121223857885bc1aad492ef85ee62a7c
Eric Saxby <esaxby@walmart.com>
2010-04-22 08:57:34 -0700

    Answered questions pagination fixes

--- app/controllers/community/answers_controller.rb
+++ app/controllers/community/answers_controller.rb
@@ -1,5 +1,7 @@
 class Community::AnswersController < ApplicationController
+  respond_to :html, :xml, :js
+
   def index
-    @questions = Question.answered.paginated(params[:page]).by_points
+    @questions = Question.answered.paginated(params[:page]).by_points.all
   end
 end
