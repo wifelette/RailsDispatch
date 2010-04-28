fe053d9fa4c8a42c693a1e602246e95b055c0933
wycats <wycats@gmail.com>
2010-04-28 00:09:44 -0700

    Refactor some view stuff

--- app/helpers/community/answers_helper.rb
+++ app/helpers/community/answers_helper.rb
@@ -1,2 +1,13 @@
 module Community::AnswersHelper
+  def question_class_for(kind)
+    active = if kind == "answers"
+      controller_name == "answers"
+    elsif kind == "new"
+      action_name == "new"
+    elsif kind == "index"
+      controller_name == "questions" && action_name == "index"
+    end
+
+    active && "active"
+  end
 end
