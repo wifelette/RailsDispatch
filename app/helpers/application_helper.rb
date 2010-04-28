30cbf4d5a445b5730d6b3ed84b3930f132a6713d
wycats <wycats@gmail.com>
2010-04-27 23:54:05 -0700

    Move community into its own tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -45,18 +45,18 @@ module ApplicationHelper
 
   def weeks_ago_in_words(from_time, include_seconds = false)
     weeks_ago = (Time.now - from_time) / 1.week
-    ["this week", "last week", "two weeks ago", 
-     "three weeks ago", "four weeks ago", 
+    ["this week", "last week", "two weeks ago",
+     "three weeks ago", "four weeks ago",
      "one month ago", "about one month ago"][weeks_ago] || super
   end
-  
+
   # def weeks_until_in_words(from_time, include_seconds = false)
   #   to_time   = Time.now
   #   weeks_ago = (from_time - to_time)/1.week
   #   ["next week", "in about two weeks", "in three weeks", "in four weeks", "in about a month", "upcoming"][weeks_ago] || distance_of_time_in_words(from_time, Time.now, include_seconds)
   # end
-  
-  
+
+
   def tracking_code
     if controller.controller_name == 'posts' && controller.action_name = 'welcome'
       render 'layouts/shared/tracking_code_home'
@@ -64,7 +64,7 @@ module ApplicationHelper
       render 'layouts/shared/tracking_code_all_pages'
     end
   end
-  
+
   def pagination
     content_tag :div, :id => 'pagination' do
       link_to "?page=#{(params[:page].to_i + 1)}", :id => "load_more" do
@@ -72,16 +72,20 @@ module ApplicationHelper
       end
     end
   end
-  
+
   def on_posts?
     controller.controller_name == "posts"
   end
-  
+
   def on_contributors?
     controller.controller_name == "contributors"
   end
-  
+
   def on_community?
-    controller.controller_name == "community" || controller.controller_name == "questions" || controller.controller_name == "answers"
+    controller.controller_name == "community"
+  end
+
+  def on_qa?
+    controller.controller_name == "questions" || controller.controller_name == "answers"
   end
 end
