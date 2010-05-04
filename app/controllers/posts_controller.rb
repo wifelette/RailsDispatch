0b431d8010efdc5bb4ee729891062d2f4f474c0e
Joseph Silvashy <jpsilvashy@gmail.com>
2010-05-04 13:18:52 -0700

    css fixes for min-height of body on certain pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
 
   def index
     @nav_posts = Post.recent
-
     @posts = Post.past
     @posts = @posts.slugged(params[:slug]) if params[:slug]
     @upcoming = Post.next
@@ -11,18 +10,16 @@ class PostsController < ApplicationController
 
   def show
     @nav_posts = Post.recent
-    
+    @upcoming = Post.next
     @post = Post.slugged(params[:slug]) if params[:slug]
     respond_with @post
   end
 
   def home
     @nav_posts = Post.recent
-
     @body_posts = Post.past
     @body_posts = @body_posts.slugged(params[:slug]) if params[:slug]
     @body_posts = @body_posts.limit(1) unless request.format.rss?
-
     @upcoming = Post.next
     respond_with @body_posts
   end
